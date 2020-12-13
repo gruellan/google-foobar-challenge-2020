11baf4d62757bf78333ec72f303ebfa8c55d6784
George Ruellan <ruellan.george@gmail.com>
2020-12-13 21:16:17 +0000

    Make it all look pretty

--- level2-part1.py
+++ level2-part1.py
@@ -2,14 +2,12 @@ from itertools import combinations
 
 
 def solution(l):
-
     l.sort(reverse=True)
     for i in reversed(range(1, len(l)+1)):
         for comb in combinations(l, i):
-            if(sum(comb) % 3 == 0):
+            if sum(comb) % 3 == 0:
                 return int(''.join(map(str, comb)))
     return 0
 
 
-if __name__ == "__main__":
-    print(solution([3, 1, 4, 1]))
+print(solution([3, 1, 4, 1]))
